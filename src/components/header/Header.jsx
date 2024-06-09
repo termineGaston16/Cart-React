6d18ae71d6c82d8df1de87ba8cfd26f86bbb26a7
Gaston Termine <gastontermine16@gmail.com>
2024-06-09 11:29:14 -0300

    as

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,13 +2,14 @@ import Cart from "./Cart";
 import NavBar from "./NavBar";
 import Search from "./Search";
 import "../../css/header.css"
+import logoDeLaPagina from "../../img/logoDeLaPagina.png"
 import { Link } from "react-router-dom";
 
 export default function Header() {
     return (<>
         <header className="header-index">
             <div>
-                <Link className="header-index-link" to={"/"}><img src="./././img/logoDeLaPagina.png" alt="logo-principal-del-ecommerce" className="header-index-logo-principal-img" /></Link>
+                <Link className="header-index-link" to={"/"}><img src={logoDeLaPagina} alt="logo-principal-del-ecommerce" className="header-index-logo-principal-img" /></Link>
                 <div className="div-search-and-cart">
                     <Search />
                     <Cart />
